a445f9f78c14e95d5945da032c3bb27f514e7e88
Mateus Sobreira <matheus.sobreira.27@gmail.com>
2022-07-26 18:40:03 -0300

    trying to fix images

--- components/ImageScrollBar.jsx
+++ components/ImageScrollBar.jsx
@@ -8,12 +8,13 @@ const LeftArrow = () => {
   const { scrollPrev } = useContext(VisibilityContext);
 
   return (
-    <Flex justifyContent={'center'} alignItems="center" marginRight={1}>
+    <Flex justifyContent="center" alignItems="center" marginRight="1">
       <Icon
         as={FaArrowAltCircleLeft}
         onClick={() => scrollPrev()}
         fontSize="2xl"
-        cursor={'pointer'}
+        cursor="pointer"
+        d={['none', 'none', 'none', 'block']}
       />
     </Flex>
   );
@@ -23,38 +24,44 @@ const RightArrow = () => {
   const { scrollNext } = useContext(VisibilityContext);
 
   return (
-    <Flex justifyContent={'center'} alignItems="center" marginRight={1}>
+    <Flex justifyContent="center" alignItems="center" marginLeft="1">
       <Icon
         as={FaArrowAltCircleRight}
         onClick={() => scrollNext()}
         fontSize="2xl"
-        cursor={'pointer'}
+        cursor="pointer"
+        d={['none', 'none', 'none', 'block']}
       />
     </Flex>
   );
 };
-
-const ImageScrollBar = ({ data }) => (
-  <ScrollMenu
-    LeftArrow={LeftArrow}
-    RightArrow={RightArrow}
-    style={{ overflow: 'hidden' }}
-  >
-    {data &&
-      data.map(item => (
-        <Box width="910px" itemID={item.id} key={item.id} padding="1">
+export default function ImageSrollBar({ data }) {
+  return (
+    <ScrollMenu
+      LeftArrow={LeftArrow}
+      RightArrow={RightArrow}
+      style={{ overflow: 'hidden' }}
+    >
+      {data.map(item => (
+        <Box
+          key={item.id}
+          width="910px"
+          itemID={item.id}
+          overflow="hidden"
+          p="1"
+        >
           <Image
             placeholder="blur"
+            alt="property"
             blurDataURL={item.url}
             src={item.url}
             width={1000}
             height={500}
-            alt="property"
-            sizes="(max-width:500px) 100px, (max-width:1023px) 400px, 1000px"
+            layout="responsive"
+            sizes="(max-width: 500px) 100px, (max-width: 1023px) 400px, 1000px"
           />
         </Box>
       ))}
-  </ScrollMenu>
-);
-
-export default ImageScrollBar;
+    </ScrollMenu>
+  );
+}
--- pages/property/[id].js
+++ pages/property/[id].js
@@ -24,56 +24,45 @@ const PropertyDetails = ({
     photos
   }
 }) => (
-  <Box maxWidth="1000px" margin="auto" p="4">
-    {photos && <ImageScrollBar data={photos} />}
-    <Box w="full" p="6">
-      <Flex paddingTop={2} alignItems="center" justifyContent={'space-between'}>
-        <Flex alignItems={'center'}>
-          <Box paddingRight={3} color="green.400">
-            {isVerified && <GoVerified />}
-          </Box>
-          <Text fontWeight={'bold'} fontSize="lg">
-            AED {millify(price)}
-            {rentFrequency && `/${rentFrequency}`}
-          </Text>
-        </Flex>
-        <Box>
-          <Avatar size={'sm'} src={agency?.logo?.url} />
-        </Box>
-      </Flex>
-      <Flex
-        align={'center'}
-        p="1"
-        justifyContent={'space-between'}
-        w="250px"
-        color={'blue.400'}
-      >
-        {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)}sqft{' '}
-        <BsGridFill />
-      </Flex>
-      <Box mt="2">
-        <Text fontFamily={'lg'} mb="2" fontWeight={'bold'}>
-          {title}
-        </Text>
-        <Text lineHeight={2} color="gray.600">
-          {description}
-        </Text>
-      </Box>
-      <Flex
-        flexWrap={'wrap'}
-        textTransform="uppercase"
-        justifyContent={'space-between'}
-      >
+    <Box maxWidth="1000px" margin="auto" p="4">
+      {photos && <ImageScrollBar data={photos} />}
+      <Box w="full" p="6">
         <Flex
+          paddingTop={2}
+          alignItems="center"
           justifyContent={'space-between'}
-          w="400px"
-          borderBottom={'1px'}
-          borderColor="gray.100"
-          padding={3}
         >
-          <Text>Type</Text>
-          <Text fontWeight={'bold'}>{type}</Text>
+          <Flex alignItems={'center'}>
+            <Box paddingRight={3} color="green.400">
+              {isVerified && <GoVerified />}
+            </Box>
+            <Text fontWeight={'bold'} fontSize="lg">
+              AED {millify(price)}
+              {rentFrequency && `/${rentFrequency}`}
+            </Text>
+          </Flex>
+          <Box>
+            <Avatar size={'sm'} src={agency?.logo?.url} />
+          </Box>
         </Flex>
+        <Flex
+          align={'center'}
+          p="1"
+          justifyContent={'space-between'}
+          w="250px"
+          color={'blue.400'}
+        >
+          {rooms} <FaBed /> | {baths} <FaBath /> | {millify(area)}sqft{' '}
+          <BsGridFill />
+        </Flex>
+        <Box mt="2">
+          <Text fontFamily={'lg'} mb="2" fontWeight={'bold'}>
+            {title}
+          </Text>
+          <Text lineHeight={2} color="gray.600">
+            {description}
+          </Text>
+        </Box>
         <Flex
           flexWrap={'wrap'}
           textTransform="uppercase"
@@ -86,50 +75,65 @@ const PropertyDetails = ({
             borderColor="gray.100"
             padding={3}
           >
-            <Text>Purpose</Text>
-            <Text fontWeight={'bold'}>{purpose}</Text>
+            <Text>Type</Text>
+            <Text fontWeight={'bold'}>{type}</Text>
           </Flex>
-        </Flex>
-        {furnishingStatus && (
           <Flex
+            flexWrap={'wrap'}
+            textTransform="uppercase"
             justifyContent={'space-between'}
-            w="400px"
-            borderBottom={'1px'}
-            borderColor="gray.100"
-            padding={3}
           >
-            <Text>Furnishing Status</Text>
-            <Text fontWeight={'bold'}>{furnishingStatus}</Text>
+            <Flex
+              justifyContent={'space-between'}
+              w="400px"
+              borderBottom={'1px'}
+              borderColor="gray.100"
+              padding={3}
+            >
+              <Text>Purpose</Text>
+              <Text fontWeight={'bold'}>{purpose}</Text>
+            </Flex>
           </Flex>
-        )}
-      </Flex>
-      <Box>
-        {amenities.length && (
-          <Text fontSize={'2xl'} fontWeight="black" mt={5}>
-            Amenities
-          </Text>
-        )}
-        <Flex flexWrap={'wrap'}>
-          {amenities.map(item =>
-            item.amenities.map(amenity => (
-              <Text
-                fontWeight={'bold'}
-                color="blue.400"
-                fontSize="lg"
-                p="2"
-                bg="gray.200"
-                m="1"
-                borderRadius={5}
-                key={amenity.text}
-              >
-                {amenity.text}
-              </Text>
-            ))
+          {furnishingStatus && (
+            <Flex
+              justifyContent={'space-between'}
+              w="400px"
+              borderBottom={'1px'}
+              borderColor="gray.100"
+              padding={3}
+            >
+              <Text>Furnishing Status</Text>
+              <Text fontWeight={'bold'}>{furnishingStatus}</Text>
+            </Flex>
           )}
         </Flex>
+        <Box textAlign='center'>
+          {amenities.length && (
+            <Text fontSize={'2xl'} fontWeight="black" mt={5}>
+              Amenities
+            </Text>
+          )}
+          <Flex flexWrap={'wrap'} alignItems='center' justifyContent='center'>
+            {amenities.map(item =>
+              item.amenities.map(amenity => (
+                <Text
+                  fontWeight={'bold'}
+                  color="blue.400"
+                  fontSize="lg"
+                  p="2"
+                  bg="gray.200"
+                  m="1"
+                  borderRadius={5}
+                  key={amenity.text}
+                >
+                  {amenity.text}
+                </Text>
+              ))
+            )}
+          </Flex>
+        </Box>
       </Box>
     </Box>
-  </Box>
 );
 
 export default PropertyDetails;
